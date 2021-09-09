2b4a0b5a4808c35643c685d0824fe6121f40b1f5
Jademir de Moura <jademirf@gmail.com>
2021-09-08 21:06:22 -0300

    add sequelize connection

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const express = require('express')
 //cria o app do tipo express()
 const app = express()
 
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
+});
+
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
 //define a porta do app como a do ambiente ou a 3003
 const port = process.env.PORT || 3003
 
